c128e408e453f18062a41b2dd1f0dd2deee2bdd4
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-08-14 01:13:24 -0500

    🐛 mostrar video en celus

--- src/programa.ts
+++ src/programa.ts
@@ -11,28 +11,28 @@ const mensaje2 = document.getElementById('mensaje2') as HTMLDivElement;
 const mensaje3 = document.getElementById('mensaje3') as HTMLDivElement;
 const mensaje4 = document.getElementById('mensaje4') as HTMLDivElement;
 const mensaje5 = document.getElementById('mensaje5') as HTMLDivElement;
-
 const mensajes = [mensaje1, mensaje2, mensaje3, mensaje4, mensaje5];
+let reproduciendoVideo = false;
 
 if (video && reproducir && audio) {
+  video.loop = true;
+
   reproducir.onclick = () => {
     if (entrada) entrada.classList.add('oculto');
-
     audio.loop = true;
-
     audio.play();
     mensajes[0].classList.add('visible');
+
     let viendoMensaje = 0;
-    let reproduciendoVideo = false;
+    try {
+      video.classList.remove('oculto');
+      video.play();
+      reproduciendoVideo = true;
+    } catch (error) {
+      console.error('Error para reproducir video', error);
+    }
 
     setInterval(() => {
-      if (!reproduciendoVideo) {
-        video.classList.remove('oculto');
-        video.loop = true;
-        video.play();
-        reproduciendoVideo = true;
-      }
-
       mensajes[viendoMensaje].classList.remove('visible');
       viendoMensaje = (viendoMensaje + 1) % mensajes.length;
       mensajes[viendoMensaje].classList.add('visible');
